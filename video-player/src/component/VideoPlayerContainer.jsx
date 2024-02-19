04aefe895ed4420e2cd727cffdfc0ab704cb9902
NishantKumar-007 <ursnishant4u@gmail.com>
2024-02-20 02:50:42 +0530

    playlist reorder feature added

--- video-player/src/component/VideoPlayerContainer.jsx
+++ video-player/src/component/VideoPlayerContainer.jsx
@@ -1,15 +1,37 @@
 import VideoPlayer from "./VideoPlayer";
 import { videoDataState } from "../store/atoms/videoDataState";
-import { useRecoilValue, useRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import { selectedVideoState } from "../store/atoms/selectedVideoState";
+import { useRef } from "react";
 
 const VideoPlayerContainer = () => {
-  const videoData = useRecoilValue(videoDataState);
+  const [videoData, setVideoData] = useRecoilState(videoDataState);
   const [selectedVideo, setSelectedVideo] = useRecoilState(selectedVideoState);
-  const handleVideoClick = (video) => {
-    // navigate("/player");
-    setSelectedVideo(video);
+
+  const draggedFrom = useRef(null);
+  const draggedTo = useRef(null);
+
+  const handleDragStart = (index) => {
+    draggedFrom.current = index;
   };
+
+  const handleDragEnter = (index) => {
+    draggedTo.current = index;
+  };
+
+  const handleDragEnd = () => {
+    const videoDataClone = [...videoData];
+    const temp = videoDataClone[draggedFrom.current];
+    videoDataClone[draggedFrom.current] = videoDataClone[draggedTo.current];
+    videoDataClone[draggedTo.current] = temp;
+    setVideoData(videoDataClone);
+  };
+
+  const handleVideoClick = (video) => {
+    setSelectedVideo(video);
+    // Additional logic for handling video click
+  };
+
   return (
     <div className="p-5 lg:flex lg:justify-evenly text-white">
       <VideoPlayer src={selectedVideo.sources}></VideoPlayer>
@@ -20,15 +42,21 @@ const VideoPlayerContainer = () => {
           <div
             key={index}
             draggable
+            onDragStart={() => handleDragStart(index)}
+            onDragEnter={() => handleDragEnter(index)}
+            onDragOver={(e) => e.preventDefault()}
+            onDragEnd={handleDragEnd}
+            onClick={() => handleVideoClick(video)} // Separate onClick for video click
             className={`playlist-item lg:w-full   cursor-pointer my-2 bg-white/15 rounded-lg p-4 hover:bg-slate-100/35 ${
               selectedVideo.title === video.title ? "selected" : ""
             }`}
           >
-            <div onClick={() => handleVideoClick(video)}>{video.title}</div>
+            <div>{video.title}</div>
           </div>
         ))}
       </div>
     </div>
   );
 };
+
 export default VideoPlayerContainer;
